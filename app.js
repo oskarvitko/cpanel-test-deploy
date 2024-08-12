a34c369846a4efb3df8a6567eaa2ee722140a068
oskarvitko <oskarvitko16@gmail.com>
2024-08-12 18:15:31 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const exec = require('child_process').exec
+
+const runBashCommand = (command) =>
+    new Promise((res, rej) => {
+        exec(command, (error, log) => {
+            if (!error) {
+                console.log(log)
+                res()
+            } else {
+                rej()
+            }
+        })
+    })
 
 const PORT = process.env.PORT || 8080
 
 app.get('/app', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/deploy', async () => {
+    console.log('Deploy Started...')
+    await runBashCommand(
+        'cd /hosting1/benzoxby/repositories/cpanel-test-deploy && git pull origin master',
+    )
+    await runBashCommand('npm install --production')
+    await runBashCommand('touch /tmp/restart.txt')
+    console.log('Deploy Ends Successfully')
 })
 
 app.listen(PORT, () => {
